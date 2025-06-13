77010cd076e791cdba83eee4dabde7fd5274daf3
Jigar Varmora <102030526+varmoraj@users.noreply.github.com>
2025-06-13 22:00:54 +0530

    Create template.js

---
+++ template.js
@@ -0,0 +1,202 @@
+// This contains the complete rental agreement template
+const agreementTemplate = `
+**RENTAL AGREEMENT**
+
+This agreement made at **[city]** on this
+**[make_date]** between,
+
+Office Lessor: [Lessor_name]
+
+**[First Party through its owner:-]{.underline}**
+
+1.  **[Lessor_name]**
+
+> Aged [Lessor_age] Years, [Lessor_address]
+
+Office Lessee: [firm_name]
+
+**[Second Party through its Partners:-]{.underline}**
+
+1.  [Lessee_1_Name]
+
+Aged [Lessee_1_age] Years, Residing At [Lessee_1_address]
+
+2.  [Lessee_2_Name]
+
+Aged [Lessee_2_age] Years, Residing At [Lessee_2_address]
+
+3.  [Lessee_3_Name]
+
+Aged [Lessee_3_age] Years, Residing At [Lessee_3_address]
+
+The [property_type] will be carried on [property_address] /or at
+such other place or Places, hereinafter referred to as the \`LESSEE' of
+the other part;
+
+WHEREAS the Lessors the lawful owner of , and otherwise well
+Sufficiently entitled [property_address] falling in the category,
+commercial office with an extent of **100.60 SQF** here in after
+referred to as the said premises AND WHEREAS at the request of the
+Lessee, the Lessor has agreed to let the said premises to the tenant for
+a term of [duration] Months commencing from date of [start_date] to
+[end_date] in the manner hereinafter appearing.
+
+**NOW THIS AGREEMENT WITNESSETH AND IT IS HEREBY AGREED BY AND BETWEEN**
+
+**[THE PARTIES AS UNDER]{.underline}**
+
+1.  That the Lessor hereby grant to the Lessee, the right to enter into
+    and use and remain in the said premises along with the existing
+    fixtures and fittings listed in Annexure 1 to this Agreement and
+    that the Lessee shall be entitled to peacefully possess, and enjoy
+    possession of the said premises, and the other rights herein.
+
+2.  That the lease hereby granted shall, unless cancelled earlier under
+    and provision of this Agreement, remain in force for a period of
+    **[duration Months.]{.underline}**
+
+3.  That the Lessee will have the option to terminate this lease by
+    giving one months' notice in writing to the Lessor.
+
+4.  That the Lessee shall have no right to create any sub-lease or
+    assign or transfer in any manner the lease or give to any one the
+    possession of the said premises, or any part thereof.
+
+5.  That the Lessee shall use the said premises only for business
+    Purposes. The Lessee shall use premises for [business_type]**.**
+
+6.  That the Lessor shall, before handing over the said premises, ensure
+    the working of sanitary, electrical and water supply Connections and
+    other fittings pertaining to the said premises. It is agreed that it
+    shall be the responsibility of the Lessor for their returning the
+    working condition of the time of re-possession of the said Premises
+    (reasonable wear and tear and loss or damage by fire, flood, rains,
+    accident, irresistible force or act of God excepted).
+
+7.  That the lessee is not authorized to make any alteration in the
+    construction of the said premises. The Lessee may however install
+    and remove his own fittings and fixtures, provided this is done
+    without causing any excessive damage or loss to the said premises.
+
+8.  That the day to day repair jobs such as fuse blow out, replacement
+    of light bulbs/tubes, leakage of water taps, maintenance of the
+    water pump and other minor repairs, etc., shall be effected by the
+    Lessee at its own cost, and any major repairs, either structural or
+    to the electrical or water connection. Plumbing leaks, water seepage
+    shall be attended to by the Lessor. In the event of the Lessor
+    failing to carry out the repairs on receiving notice from the
+    lessee, the lessee shall undertake the necessary repairs and the
+    lessor will be liable to immediately reimburse costs incurred by the
+    Lessee
+
+9.  That the lessor or its duly authorized agent shall have the right to
+    enter into or upon the said premises or any part thereof at a
+    mutually arranged convenient time for the purpose of inspection.
+
+10. That the Lessee shall use the said premises along with its fixtures
+    and fitting in careful and responsible manner and shall handover the
+    premises to the lessor in working condition (reasonable wear and
+    tear and loss or damage by fire, flood, ranis, accidents,
+    irresistible force or act of God excepted).
+
+11. That in consideration of use of the said premises the lessee agrees
+    that he shall pay to the Lessor during the period of this agreement,
+    a monthly rent at the rate of Monthly Rental is
+    **Rs.[rent].** The amount will be paid in advance on or before the date of 1^st^ day of every English calendar month
+    unless as per mutual understanding between lessee and lessor.
+
+12. It is hereby agreed that if default is made by the lessee in payment
+    of the rent for a period of three months, or inobservance and
+    performance of any of the covenants and stipulations hereby
+    contained and on the part to be observed and performed by the
+    lessee, then on such default, the lessor shall be entitled in
+    addition to or in the alternative to any other remedy that may be
+    available to him at this discretion, to terminate the lease and
+    eject the lessee from the said premises; and to take possession
+    thereof as full and absolute owner thereof, provided that a notice
+    in writing shall be given by the lessor to the lessee of his
+    intention to terminate the lease and to take possession of the said
+    premises. If the arrears of rent are paid or the lessee comply with
+    or carry out the covenants and conditions or stipulations, Within
+    fifteen days from the service of such notice, then the lessor shall
+    not be entitled to take possession of the said premises.
+
+13. That in addition to the compensation mentioned above, the Lessee
+    shall pay the actual electricity, shared maintenance, Water bills
+    for the period of the agreement directly to the Authorities
+    concerned.
+
+14. That the Lessor shall be responsible for the payment of all taxes
+    and levies pertaining to the said premises including but not limited
+    to House Tax, Property Tax, other cesses, if any, and any other
+    statutory taxes, levied by the Government or Governmental
+    Departments. During the term of this Agreement, the Lessor shall
+    comply with all rules, regulations and requirements of any statutory
+    authority, local, state and central government and governmental
+    departments in relation to the said premises.
+
+15. The Lessor **[Lessor_name]** Owner of the property
+    have permitted and allowed Lessee **[firm_name]**
+    and **[Partners]{.underline}** for operating and conducting their
+    business from the above business address and to obtain registration
+    under
+
+**[Gst Number, PGVCL connection, Trade Mark, Udyam Registration, Income
+Tax, Bank, and Other Government Registration for the above business
+address.]{.underline}**
+
+**IN WITNESS WHEREOF, the parties hereto have set their hands. On the
+day and year first hereinabove mentioned.**
+
++--------------------------------------------------+-------------------+
+| **\                                              | **[Pho            |
+| Office Lessor [Lessor_name]**                  | to]{.underline}** |
+|                                                  |                   |
+| **First Party as an Owner:-**                    |                   |
++:=================================================+:==================+
+| **............................                   |                   |
+| ..............................................** |                   |
+|                                                  |                   |
+| 1.  **[Lessor_name]**                          |                   |
++--------------------------------------------------+-------------------+
+|                                                  |                   |
++--------------------------------------------------+-------------------+
+| **Office Lessee**                                |                   |
+|                                                  |                   |
+| **M/s. [firm_name]**                           |                   |
+|                                                  |                   |
+| **Second Party through its Partners:-**          |                   |
++--------------------------------------------------+-------------------+
+| **.............................                  |                   |
+| ..............................................** |                   |
+|                                                  |                   |
+| **1. [Lessee_1_Name]**                         |                   |
++--------------------------------------------------+-------------------+
+| **.............................                  |                   |
+| ..............................................** |                   |
+|                                                  |                   |
+| **2. [Lessee_2_Name]**                         |                   |
++--------------------------------------------------+-------------------+
+| **.............................                  |                   |
+| ..............................................** |                   |
+|                                                  |                   |
+| **3. [Lessee_3_Name]**                         |                   |
++--------------------------------------------------+-------------------+
+
+**Witness:-**
+
+**Name & Address Signature**
+
+1.  **[Witness1]**
+    ......................................................................
+
+2.  **[Witness2]**
+    ...................................................................................
+
+**RENT AGREEMENT**
+
+**Property Address:**
+
+> Vagepar Survey No.919p, Plot No.3, Plot No.4, Plot No.5, Office
+> No.2040, 2nd Floor,City Mall Nr. Sanala Baypass, Bhaktinagar Circle,
+> Morbi-
